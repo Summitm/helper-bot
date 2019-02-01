d31207116d3fb16f1ccc075d52996207638e3d25
Summitm <summitros@gmail.com>
2019-02-02 00:54:49 +0300

    first trial

--- matchday_twitter_bot.py
+++ matchday_twitter_bot.py
@@ -3,3 +3,18 @@
 import tweepy
 
 print("Hello! I am a twitter bot")
+
+CONSUMER_API_KEY 	= "axZ2zerBvFN3LNiDtguq78Kc4"
+CONSUMER_API_SECRET = "srY2PdGOZJSsuQr7yDCFukhxAG3AMOf4e70p84UpJl3dedZ99k" 
+ACCESS_TOKEN = "1091001075048566785-Rzbt4MQjzpdcGrG4fXKXsFMiY5bjgJ"
+ACCESS_TOKEN_SECRET = "mPblWwMd4fCY1pqWO8WxaxrzMEnO6brduzJZJzABU1zHQ"
+
+auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+tweets = api.home_timeline()
+
+for tweet in tweets:
+	print(twee.text)
